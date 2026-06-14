ce5a4aecf0359f6a7769e2ca10f3bc55fd76e020
Wisnu Box <hwisnu222@gmail.com>
2026-06-14 18:00:51 +0700

    fix: hostname column

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::{net::{IpAddr, Ipv4Addr}, thread, time::Duration};
+use std::{collections::HashMap, net::{IpAddr, Ipv4Addr}, thread, time::Duration};
 
 use dns_lookup::lookup_addr;
 use netdev::ipnet::Ipv4Net;
 use netscan::{host::Host, scan::{scanner::HostScanner, setting::{HostScanSetting, HostScanType}}};
+use tabled::{Table, Tabled, settings::Style};
+
+#[derive(Tabled)]
+struct HostName{
+    ip_address: String,
+    host: String,
+    ttl: u8,
+}
 
 fn main() {
     let interface = netdev::get_default_interface().unwrap();
@@ -17,6 +25,7 @@ fn main() {
     let nw_addr = Ipv4Net::new(net.network(), 24).unwrap();
 
     let mut handles = vec![];
+    let mut ipv4_list = HashMap::new();
 
     for host in nw_addr.hosts(){
         let ipv4 = IpAddr::V4(host);
@@ -24,8 +33,9 @@ fn main() {
         let handle = thread::spawn(move ||{
             let hostname = match lookup_addr(&ipv4) {
                 Ok(name)=> name,
-                Err(_) => format!("unknow-host-{}", host)
+                Err(_) => "unknown".to_string() 
             };
+
 
             (ipv4, hostname)
         });
@@ -35,6 +45,7 @@ fn main() {
 
     for handle in handles{
         if let Ok((ipv4, hostname)) = handle.join(){
+            ipv4_list.insert(ipv4, hostname.clone());
             scan_setting.add_target(Host::new(ipv4, hostname));
         }
     }
@@ -42,8 +53,19 @@ fn main() {
     let result = HostScanner::new(scan_setting).scan();
 
     println!("Status: {:?}", result.scan_status);
-    println!("Up hosts:");
+    let mut hostnames = vec![];
+
     for host in result.hosts{
-        println!("{:?}", host);
+        let hostname = ipv4_list.get(&host.ip_addr).map_or("unknown".to_string(), |x| x.to_string());
+
+        hostnames.push(HostName{
+            ip_address: host.ip_addr.to_string(),
+            host: hostname,
+            ttl: host.ttl
+        });
     }
+    
+    let mut table = Table::new(hostnames);
+    table.with(Style::modern());
+    println!("{}", table);
 }
